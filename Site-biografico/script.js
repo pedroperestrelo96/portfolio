879902f6ea397ab50e8d9bbd0323f4808a30172b
pedroperestrelo96 <perestrelopedro03@gmail.com>
2024-12-15 23:31:45 +0000

    Add files via upload

---
+++ Site-biografico/script.js
@@ -0,0 +1,5 @@
+// Exemplo de funcionalidade simples
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Obrigado por entrar em contacto!');
+});
